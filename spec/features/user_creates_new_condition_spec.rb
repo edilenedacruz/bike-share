24e916d8e140dc6a543025b83c599322df54c4af
Alex Fosco <alex.hf88@gmail.com>
2017-02-01 22:20:22 -0700

    fixes and adds feature and model tests

--- spec/features/user_creates_new_condition_spec.rb
+++ spec/features/user_creates_new_condition_spec.rb
@@ -13,10 +13,9 @@ RSpec.describe "User inputs weather conditions for a day" do
     fill_in('condition[mean_visibility_miles]', with: 55)
     fill_in('condition[mean_wind_speed_mph]', with: 55)
     fill_in('condition[precipitation_inches]', with: 55)
-    click_button('Update Condition Report')
+    click_button('Create Condition Report')
 
-    #save_and_open_page
-    #expect(page).to have_current_path("/conditions/#{@condition.id}")
+    save_and_open_page
     expect(page).to have_content("7/29/2013")
     expect(page).to have_content(55)
   end
--- spec/models/condition_spec.rb
+++ spec/models/condition_spec.rb
@@ -1,30 +1,30 @@
 require_relative '../spec_helper'
 
 RSpec.describe Condition do
+  it "is valid with all attributes" do
+    condition = Condition.create(date: "11/02/2012",
+                                 max_temperature_f: 12,
+                                 mean_temperature_f: 55,
+                                 min_temperature_f: 46,
+                                 mean_humidity: 32,
+                                 mean_visibility_miles: 21,
+                                 mean_wind_speed_mph: 12,
+                                 precipitation_inches: 0,
+                                 )
 
-  describe 'validations' do
-    it "is valid with valid parameters" do
-      condition = Condition.new(date: 9/2/2013,
-                                max_temperature_f: 78,
-                                mean_temperature_f: 73,
-                                min_temperature_f: 67,
-                                mean_humidity: 43,
-                                mean_visibility_miles: 11,
-                                mean_wind_speed_mph: 12,
-                                precipitation_inches: 0)
-      expect(condition).to be_valid
-    end
-
-    it "validates presence of date" do
-      condition = Condition.new(max_temperature_f: 78,
-                                mean_temperature_f: 73,
-                                min_temperature_f: 67,
-                                mean_humidity: 43,
-                                mean_visibility_miles: 11,
-                                mean_wind_speed_mph: 12,
-                                precipitation_inches: 0)
-      expect(condition).to_not be_valid
-    end
+    expect(condition).to be_valid
   end
-  
+
+  it "is not valid without a date" do
+    condition = Condition.create(max_temperature_f: 12,
+                                 mean_temperature_f: 55,
+                                 min_temperature_f: 46,
+                                 mean_humidity: 32,
+                                 mean_visibility_miles: 21,
+                                 mean_wind_speed_mph: 12,
+                                 precipitation_inches: 0,
+                                 )
+
+    expect(condition).to_not be_valid
+  end
 end
